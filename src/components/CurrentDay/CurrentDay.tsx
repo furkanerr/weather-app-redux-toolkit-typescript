493863cc474ec13adcc3ee3d6afb377fc2ef2588
furkaner <furkan08_@hotmail.com>
2022-06-11 00:32:58 +0300

    Create CurrentDay.tsx
    weather infos displayed in the component and degree conversion added

---
+++ src/components/CurrentDay/CurrentDay.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styles from "./CurrentDay.module.css";
+import locationIcon from "../../Constants/Assets/location-pin.png";
+const CurrentDay: React.FC<{
+  temp: number | undefined;
+  weather: string | undefined;
+  name: string | undefined;
+}> = ({ temp, name, weather }) => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const d = new Date();
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth() + 1];
+  let celcius = Math.round(temp! - 273.15);
+  return (
+    <div className={styles.Container}>
+      <div className={styles.img}></div>
+
+      <article className={styles.top}>
+        <div className={styles.today}>{day}</div>
+        <div className={styles.date}>
+          {month} {date}
+        </div>
+        <div className={styles.locationDiv}>
+          <img src={locationIcon} className={styles.locationImg}></img>
+          <div className={styles.City}>{name}</div>
+        </div>
+      </article>
+      <article className={styles.bottom}>
+        <div className={styles.temp}>{celcius}°C</div>
+        <div className={styles.description}>{weather}</div>
+      </article>
+    </div>
+  );
+};
+
+export default CurrentDay;
