f9ac29150068a8c7af016590c6faa3ded8d8b6ee
furkaner <furkan08_@hotmail.com>
2022-06-11 00:30:04 +0300

    Home done

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,10 +1,35 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import Service from '../../services/api'
+import styles from './Homepage.module.css'
+import Forecats from '../../components/Forecast/Forecats'
+import SearchBar from '../../components/SearchBar/SearchBar'
+import { fetchWeatherByCityName } from '../../features/weather/weatherSlice'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
 const HomePage = () => {
 
-
+  const [city,setCity] = useState<string>('')
+  const dispatch = useAppDispatch()
+  const {loading,error,weather} = useAppSelector(state => state.weather)
+  useEffect(()=>{
+    if(city){
+    dispatch(fetchWeatherByCityName(city))
+    }
+  },[city])
+  //if(loading) return <div>Loading...</div>
   return (
-    <div>HomePage</div>
+    <div className={styles.Container}>
+      <SearchBar setCity={setCity}/>
+        {
+          
+     loading === 'pending' ? <div>Loading...</div> :
+      error ? <div>Error</div> :
+      weather ? <Forecats weather={weather}/> : 'There is no location'
+      
+
+        }
+       
+      
+    </div>
   )
 }
 
