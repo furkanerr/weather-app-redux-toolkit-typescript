ff82d4ce6ccc18be965cb86a560016eea5d34acd
furkaner <furkan08_@hotmail.com>
2022-06-10 03:20:52 +0300

    reducer added

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-// ...
+import weatherReducer from '../features/weather/weatherSlice'
 
 export const store = configureStore({
   reducer: {
- 
+    weather: weatherReducer,
   },
 })
 
