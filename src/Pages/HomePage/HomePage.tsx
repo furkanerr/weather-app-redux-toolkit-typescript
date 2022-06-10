704c705ccff51b23b56eda222e5bbb59bc0b3df7
furkaner <furkan08_@hotmail.com>
2022-06-10 03:22:04 +0300

    created

---
+++ src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import React,{useEffect} from 'react'
+import Service from '../../services/api'
+const HomePage = () => {
+
+
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
---
+++ src/components/Forecast/Forecats.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Forecats = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Forecats
