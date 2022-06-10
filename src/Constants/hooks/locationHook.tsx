9c1cd87898d7ba0b8848a243d9cf233fb697365d
furkaner <furkan08_@hotmail.com>
2022-06-11 01:31:15 +0300

    custom geolocation hook added

---
+++ src/Constants/hooks/locationHook.tsx
@@ -0,0 +1,32 @@
+import React,{useState,useEffect} from 'react'
+import { Coord } from '../../Types/types'
+const useLocation = () => {
+  
+    const [position,setPosition] = useState<Coord>()
+    const [errorLocation,setError] = useState<string>('')
+    const [loadingLocation,setLoading] = useState<boolean>(false)
+
+    const onError = (error:GeolocationPositionError) => {
+        setError(error.message)
+        setLoading(false)
+    } 
+    const onSuccess = (position: GeolocationPosition) => {
+        
+        setPosition({lat:position.coords.latitude,lon:position.coords.longitude})
+        setLoading(false)
+    }
+    
+    useEffect(()=>{
+        if(navigator.geolocation){
+            setLoading(true)
+            navigator.geolocation.getCurrentPosition(
+                onSuccess,
+                onError,
+            )
+        }
+    },[])
+    
+    return {position,errorLocation,loadingLocation}
+}
+
+export default useLocation
